75f196c34fe7e6aeb86dae6b82427d812de58d2d
Sergio Zambrano <sergiouce16@gmail.com>
2025-06-30 04:14:24 -0500

    Added DB Context

--- Persistence/DependencyInjection.cs
+++ Persistence/DependencyInjection.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
 
 namespace Persistence
 {
-    internal class DependencyInjection
+    public static class DependencyInjection
     {
+        /// <summary>
+        /// Initialize all the Persistence services
+        /// </summary>
+        /// <param name="services">Contract collection of service descriptor</param>
+        /// <param name="configuration">App configuration</param>
+        /// <returns>Contract collection of service descriptor</returns>
+        /// 
+        public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionStrings = configuration.GetSection("ConnectionStrings").Get<ConnectionStrings>();
+
+            // Add db context
+            services.AddDbContext<SqlServerDbContext>(options => options.UseSqlServer(connectionStrings.DefaultConnection));
+
+            return services;
+        }
     }
 }
--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -1,21 +1,19 @@
-using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Append DB Context
-var connectionString = builder.Configuration["Persistence:ConnectionStrings:DefaultConnection"];
-builder.Services.AddDbContext<SqlServerDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+// Inject dependencies
+var persistenceConfiguration = builder.Configuration.GetSection("Persistence");
+builder.Services.AddPersistence(persistenceConfiguration);
 
+//
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
-// Creates DB if not exists
+// Create DB if not exists
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<SqlServerDbContext>();
@@ -30,9 +28,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
